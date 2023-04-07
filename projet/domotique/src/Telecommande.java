57cf152d48c011f33247b74d558114216fdeb259
Gaetan <gaetan.szezcpanski1@etu.univ-lorraine.fr>
2023-04-07 09:17:59 +0200

    Ajout javaclass Telecommande

---
+++ projet/domotique/src/Telecommande.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Telecommande {
+
+    private ArrayList<Lampe> lampes;
+
+    public Telecommande(){
+        this.lampes = new ArrayList<Lampe>();
+    }
+
+    public void ajouterLampe(Lampe){
+        throw new Error("Code non écrit");
+    }
+    public void activerLampe(int){
+        throw new Error("Code non écrit");
+    }
+
+    public void desactiverLampe(int){
+        throw new Error("Code non écrit");
+    }
+
+    public void activerTout(){
+        throw new Error("Code non écrit");
+    }
+
+    public String toString(){
+        throw new Error("Code non écrit");
+    }
+}
