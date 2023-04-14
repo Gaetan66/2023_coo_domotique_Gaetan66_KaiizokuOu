62a423ada494829e5a04302728d5c9bdceb9e70f
Alexis <120113186+KaiizokuOu@users.noreply.github.com>
2023-04-14 09:36:59 +0200

    Ajout méthodes adaptateur

--- projet/domotique/src/Adaptateur.java
+++ projet/domotique/src/Adaptateur.java
@@ -3,4 +3,34 @@ import java.util.ArrayList;
 public class Adaptateur implements Appareil{
 
     private ArrayList<Cheminee> cheminees;
+
+    public Adaptateur(){
+        this.cheminees = new ArrayList<Cheminee>();
+    }
+
+    public void ajouter(Cheminee c) {
+        this.cheminees.add(c);
+    }
+
+    public void allumer() {
+        for(int i = 0; i < this.cheminees.size(); i++){
+            this.cheminees.get(i).changerIntensite(100);
+        }
+    }
+
+    public void eteindre() {
+        for(int i = 0; i < this.cheminees.size(); i++){
+            this.cheminees.get(i).changerIntensite(0);
+        }
+    }
+
+    public boolean isAllume() {
+        boolean r = false;
+        for(int i = 0; i < this.cheminees.size(); i++){
+            if (this.cheminees.get(i).getLumiere() > 0){
+                r = true;
+            }
+        }
+        return r;
+    }
 }
