a99014dbc870ae3f3f3dee51b161cd831cc716ee
Alexis <120113186+KaiizokuOu@users.noreply.github.com>
2023-05-09 14:44:08 +0200

    Add telecommande

--- projet/domotique/src/Main.java
+++ projet/domotique/src/Main.java
@@ -7,12 +7,19 @@ public class Main {
 		
 		
 		Lampe l1=new Lampe("Lampe1");
+		Hifi h1 = new Hifi();
+		Adaptateur a1 = new Adaptateur();
+		Cheminee c1 = new Cheminee();
+		a1.ajouter(c1);
 		t.ajouterAppareil(l1);
-		
+		t.ajouterAppareil(h1);
+		t.ajouterAppareil(a1);
+
 		/****** A COMPLETER ******/
 		// TODO
 		
 		TelecommandeGraphique tg=new TelecommandeGraphique(t);
+		System.out.print(tg.toString());
 		
 
 		
