e9bc03a8619e50f911337cdd10da260317047ba1
Alexis <120113186+KaiizokuOu@users.noreply.github.com>
2023-05-17 15:02:11 +0200

    SUP TEST

--- Td_CD/TD_CD/src/main/MainChargeurMagasin.java
+++ Td_CD/TD_CD/src/main/MainChargeurMagasin.java
@@ -33,8 +33,8 @@ public class MainChargeurMagasin {
 		//resultat.trierAriste();
 		//System.out.println(resultat);
 
-		resultat.TrieSelection();
-		System.out.println(resultat);
+		//resultat.TrieSelection();
+		//System.out.println(resultat);
 
 	}
 
